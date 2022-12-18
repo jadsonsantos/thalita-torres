31d5ff8aa421f031e479831685cb7d018830ff54
Jadson Santos <jadsondossantos5@gmail.com>
2022-12-18 20:37:50 -0300

    adds roboto font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.scss'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({ weight: ['400', '500'], subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
