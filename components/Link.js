22a14b360413880f1bf0676d93857515a2fd13ed
Jadson Santos <jadsondossantos5@gmail.com>
2022-12-20 20:35:01 -0300

    updates link component

--- components/Link.js
+++ components/Link.js
@@ -3,7 +3,12 @@ import { Link as NextLink } from 'next'
 const Link = ({ isExternal = false, children, ...props }) => {
   if (isExternal) {
     return (
-      <a target="blank" rel="noopener" className="external-link" {...props}>
+      <a
+        target="blank"
+        rel="noopener noreferrer"
+        className="external-link"
+        {...props}
+      >
         {children}
       </a>
     )
