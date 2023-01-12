3dddd44ea3c39440b2bf54886d01bce90ec8791d
Jadson Santos <jadsondossantos5@gmail.com>
2023-01-11 22:32:26 -0300

    services style

--- components/Services/index.js
+++ components/Services/index.js
@@ -1,5 +1,6 @@
 import styles from '../../styles/Services.module.scss'
 import { services } from './data.js'
+import Image from 'next/image'
 
 const Services = () => (
   <section className={styles.services}>
@@ -8,8 +9,15 @@ const Services = () => (
         What do I (really like to) do
       </h2>
       <ul className={styles.services__list}>
-        {services.map(({ icon, title, description }) => (
+        {services.map(({ name, title, description }) => (
           <li className={styles.services__item} key={title}>
+            <Image
+              src={`/icons/${name}.svg`}
+              alt={`${name} logo`}
+              className={styles.services__logo}
+              width={64}
+              height={64}
+            />
             <p className={styles.services__title}>{title}</p>
             <p className={styles.services__description}>{description}</p>
           </li>
