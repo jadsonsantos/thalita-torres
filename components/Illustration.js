825f3a09f06251e3751c3a7c40e55b6be4604fad
Jadson Santos <jadsondossantos5@gmail.com>
2023-01-11 22:32:54 -0300

    illustration style

--- components/Illustration.js
+++ components/Illustration.js
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import DesktopIllustration from '../public/images/illustration-desktop.png'
-// import MobileIllustration from '../public/images/illustration-mobile.png'
-
+import MobileIllustration from '../public/images/illustration-mobile.png'
+import isMobile from '../utils/isMobile'
 import styles from '../styles/Illustration.module.scss'
 
 const Illustration = () => (
   <section className={styles.illustration}>
     <Image
       className={styles.illustration__image}
-      src={DesktopIllustration}
+      src={isMobile() ? MobileIllustration : DesktopIllustration}
       alt="Ilustração"
-      priority
     />
   </section>
 )
