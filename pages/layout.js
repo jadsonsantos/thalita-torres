0c4e52f5310f0df9fbc907433d2992089da2efed
Jadson Santos <jadsondossantos5@gmail.com>
2023-01-22 15:04:15 -0300

    layout component

--- pages/layout.js
+++ pages/layout.js
@@ -1,9 +1,11 @@
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Header />
       <main>{children}</main>
       <Contact />
       <Footer />
