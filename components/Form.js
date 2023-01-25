0390943bd035aff4f2e0f279705872ead0e5fd4a
Jadson Santos <jadsondossantos5@gmail.com>
2023-01-25 18:42:24 -0300

    form component

---
+++ components/Form.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Form.module.scss'
+
+const Form = () => (
+  <section className={styles.form}>
+    <div className="container">form</div>
+  </section>
+)
+
+export default Form
