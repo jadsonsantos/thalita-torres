1da33d72ecb318bdd3251d8dea168140e6b3bb21
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-12 16:20:48 -0300

    textarea component imported

--- components/Form.js
+++ components/Form.js
@@ -1,12 +1,14 @@
 import styles from '../styles/Form.module.scss'
 import Input from './Input'
 import { useState } from 'react'
+import TextArea from './TextArea'
 
 const Form = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [company, setCompany] = useState('')
   const [source, setSource] = useState('')
+  const [message, setMessage] = useState('')
 
   return (
     <section className={styles.form}>
@@ -43,6 +45,14 @@ const Form = () => {
             type="text"
             value={source}
             setValue={setSource}
+          />
+          <TextArea
+            label="Tell me about your project"
+            placeholder="To help me understand a little bit more of your needs, and then I'll get in touch with you."
+            type="text"
+            id="message"
+            value={message}
+            setValue={setMessage}
           />
           <button className={styles.form__submit} type="submit">
             Let's do this!
