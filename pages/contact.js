e851f5bd81fc56eebf042be6142ea36bd2e2cc69
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-14 23:00:11 -0300

    contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Form from '../components/Form'
 import Introduction from '../components/Introduction'
 import Layout from './layout'
+import styles from '../styles/Contact.module.scss'
 
 export default function Contact() {
   return (
-    <Layout page="contact container">
+    <Layout className={`container ${styles.contact}`}>
       <Head>
         <title>Thalita Torres | Contact</title>
       </Head>
