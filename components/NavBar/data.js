57ff37cd0bdd51790cca6d4293f0d4de2409ac9d
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-18 10:02:21 -0300

    moves data menu to another file

---
+++ components/NavBar/data.js
@@ -0,0 +1,6 @@
+export const links = [
+  { name: 'Home', link: '/' },
+  { name: 'Work', link: '/work' },
+  { name: 'About', link: '/about' },
+  { name: 'Contact', link: '/contact' },
+]
--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,17 +1,11 @@
 import Link from 'next/link'
 import styles from './NavBar.module.scss'
-
-const data = [
-  { name: 'Home', link: '/' },
-  { name: 'Work', link: '/work' },
-  { name: 'About', link: '/about' },
-  { name: 'Contact', link: '/contact' },
-]
+import { links } from './data'
 
 const NavBar = () => (
   <nav className={styles.navbar}>
     <ul className={styles.navbar__list}>
-      {data.map(({ name, link }) => (
+      {links.map(({ name, link }) => (
         <li key={name} className={styles.navbar__item}>
           <Link href={link}>{name}</Link>
         </li>
