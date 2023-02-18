286d727e58e7cef64f96644b840968b41f0ff964
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-18 13:46:15 -0300

    menu hamburguer

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,18 +1,22 @@
-import classNames from 'classnames'
+import classNames from 'classnames/bind'
+import Image from 'next/image'
 import styles from '../styles/MenuButton.module.scss'
+import hamburguerIcon from '../public/icons/hamburguer.svg'
+import closeIcon from '../public/icons/close.svg'
 
-const CLASS_NAME = 'hamburguer-button'
-const MenuButton = ({ isActive, handleClick }) => (
-  <button
-  // className={classNames('hamburger-button', {
-  //   'hamburger-button--active': isActive,
-  // })}
-  // onClick={() => handleClick()}
-  >
-    <div className="hamburger-button__icon">
-      <div className="hamburger-button__line"></div>
-    </div>
-  </button>
-)
+const MenuButton = ({ isActive, handleClick }) => {
+  let cx = classNames.bind(styles)
+  const customClass = cx({
+    ['hamburguer-button']: true,
+    ['hamburguer-button--active']: isActive,
+  })
+  const currentIcon = isActive ? closeIcon : hamburguerIcon
+
+  return (
+    <button className={customClass} onClick={() => handleClick()}>
+      <Image src={currentIcon} alt="Menu hamburguer" />
+    </button>
+  )
+}
 
 export default MenuButton
