267ea015e340bdd2ec00dea9f82179bb2114ed3a
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-18 10:11:52 -0300

    import navbar hook

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,17 +1,22 @@
 import Link from 'next/link'
 import styles from './NavBar.module.scss'
 import { links } from './data'
+import useNavBar from './useNavBar'
 
-const NavBar = () => (
-  <nav className={styles.navbar}>
-    <ul className={styles.navbar__list}>
-      {links.map(({ name, link }) => (
-        <li key={name} className={styles.navbar__item}>
-          <Link href={link}>{name}</Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-)
+const NavBar = () => {
+  const { isActiveLink } = useNavBar()
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar__list}>
+        {links.map(({ name, link }) => (
+          <li key={name} className={styles.navbar__item}>
+            <Link href={link}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
 
 export default NavBar
