4437aac3605bebdd07b7efa3df5ca9e8d32c652b
Jadson Santos <jadsondossantos5@gmail.com>
2023-02-18 10:48:25 -0300

    adds active link feat

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -2,18 +2,27 @@ import Link from 'next/link'
 import styles from './NavBar.module.scss'
 import { links } from './data'
 import useNavBar from './useNavBar'
+import classNames from 'classnames/bind'
 
 const NavBar = () => {
   const { isActiveLink } = useNavBar()
+  let cx = classNames.bind(styles)
 
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navbar__list}>
-        {links.map(({ name, link }) => (
-          <li key={name} className={styles.navbar__item}>
-            <Link href={link}>{name}</Link>
-          </li>
-        ))}
+        {links.map(({ name, link }) => {
+          const customClass = cx({
+            navbar__item: true,
+            'navbar__item--active': isActiveLink(link),
+          })
+
+          return (
+            <li key={name} className={customClass}>
+              <Link href={link}>{name}</Link>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
